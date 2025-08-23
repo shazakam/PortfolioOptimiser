bce659327578fd23a5988ce18483a579f68844ec
Shazakam <louis.godtfredsen@gmail.com>
2025-08-23 20:10:10 +0200

    Began UI in Streamlit - Should work - Currently still loads all the data in at first go

--- utils/volatility_calculator.py
+++ utils/volatility_calculator.py
@@ -1,11 +1,11 @@
 import jax
-
+import numpy as np
 class VolatilityCalculator():
 
     def __init__(self):
         pass
 
-    def annualised_std_log_returns(self, historical_data : jax.Array, return_period : int, daily_week_monthly : str) -> float:
+    def annualised_std_log_returns(self, historical_data : np.array, return_period : int, daily_week_monthly : str) -> float:
         # Calculate Log Returns
         log_returns = jax.numpy.log(historical_data[return_period:] / historical_data[:-return_period])
 
@@ -17,29 +17,28 @@ class VolatilityCalculator():
         elif daily_week_monthly == 'monthly':
             return jax.numpy.std(log_returns)*9
 
-    def calculate_beta(self, historical_data : jax.Array, market_data : jax.Array, return_period : int) -> float:
+    def calculate_beta(self, historical_data : np.array, market_data : np.array, return_period : int) -> float:
         # Calculate Log Returns
         returns = historical_data[return_period:] / historical_data[:-return_period]
         market_returns = market_data[return_period:] / market_data[:-return_period]
 
-        print(f"Log Returns: {returns}, Market Log Returns: {market_returns}")
-
         # Calculate Covariance and Variance
-        covariance = jax.numpy.cov(returns, market_returns)[0][1]
-        variance = jax.numpy.var(market_returns)
+        covariance = np.cov(returns, market_returns)[0][1]
+        variance = np.var(market_returns)
 
         # Calculate Beta
         beta = covariance / variance
+
         return beta
     
     # Apply std Log Returns across DataFrame Columns
-    def calculate_std_log_returns_for_df(self, df, return_period, daily_week_monthly):
-        volatility_dict = {}
-        for column in df.columns:
-            if column != 'Date':
-                volatility = self.annualised_std_log_returns(jax.numpy.array(df[column].values), return_period, daily_week_monthly)
-                volatility_dict[column] = volatility
-        return volatility_dict
+    # def calculate_std_log_returns_for_df(self, df, return_period, daily_week_monthly):
+    #     volatility_dict = {}
+    #     for column in df.columns:
+    #         if column != 'Date':
+    #             volatility = self.annualised_std_log_returns(jax.numpy.array(df[column].values), return_period, daily_week_monthly)
+    #             volatility_dict[column] = volatility
+    #     return volatility_dict
     
 
     # Apply Beta Calculation across DataFrame Columns
@@ -47,6 +46,6 @@ class VolatilityCalculator():
         beta_dict = {}
         for column in df.columns:
             if column != 'Date':
-                beta = self.calculate_beta(jax.numpy.array(df[column].values), jax.numpy.array(market_data.values), return_period)
+                beta = self.calculate_beta(df[column].values, market_data.values, return_period)
                 beta_dict[column] = beta
         return beta_dict
