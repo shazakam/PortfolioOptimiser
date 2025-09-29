bdad786463136ab0bd5e79292b33460e00def866
Shazakam <louis.godtfredsen@gmail.com>
2025-09-29 21:59:48 +0100

    Renaming and Re-organising

---
+++ streamlit_app.py
@@ -0,0 +1,30 @@
+import streamlit as st 
+import pandas as pd
+import yfinance as yf
+from utils.data_utils import load_market_data, get_fundamentals
+import jax.numpy as jnp
+from utils.volatility_calculator import VolatilityCalculator
+import numpy as np 
+import plotly.express as px
+
+welcome_page = st.Page("welcome/welcome_page.py", title="Welcome", default=True)
+
+data_loading = st.Page(
+    "portfolio/1_Data_Loading_page.py", title="Data Loading", default=True)
+
+metric_filtering = st.Page("portfolio/2_Metric_Filtering_page.py", title="Metric Filtering")
+
+portfolio_modelling = st.Page(
+    "portfolio/3_Model_page.py", title="Portfolio Models")
+
+equity_analysis = st.Page("analysis/stuff.py", title="Fundamental Equity Analysis")
+
+pg = st.navigation(
+    {
+        "Welcome": [welcome_page],
+        "Portfolio Optimisation": [data_loading, metric_filtering, portfolio_modelling],
+        "Fundamental Equity Analysis": [equity_analysis]
+    }
+)
+
+pg.run()
---
+++ welcome/welcome_page.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("Financial Analysis Application")
+st.write("This application consists of two main sections: Portfolio Optimisation and Fundamental Equity Analysis. Use the sidebar to navigate between these sections and their respective pages.")
+
+
+st.write("Portfolio Optimisation includes pages for Data Loading, Metric Filtering, and Portfolio Models. Fundamental Equity Analysis provides tools for analyzing individual equities based on their fundamentals.")
+
+st.write("For the portfolio optimisation you must first load the market data, then filter the equities based on your preferences and finally select a portfolio model and view the results and visualizations of your portfolio.")
+
+st.write("FINANCIAL ANALYSIS TO BE ADDED LATER")
